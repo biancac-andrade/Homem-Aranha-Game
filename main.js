83d1bd213af092227fe9532837330c0e70fd4eb8
Bianca Cristina <bc.andrade05@gmail.com>
2023-01-24 16:47:14 -0300

    Ajuste finais

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
+var chao, chaoImg;
+var chaoInvisivel;
 
 var aranha, aranhaImg;
 
-var groundInvisive;
-
-var estado = "play";
-
 function preload() {
-  aranhaImg = loadImage("img/homem.png");
+  aranhaImg = loadImage("img/aranha.png");
+  chaoImg = loadImage("img/chao.png");
 }
 
 function setup() {
   createCanvas(1348, 630);
 
-  aranha = createSprite(620, 280, 20, 20);
-  aranha.addAnimation("aranha", aranhaImg);
-  aranha.scale = 0.5;
+  aranha = createSprite(200, 300, 20, 20);
+  aranha.addImage("aranha", aranhaImg);
+  aranha.scale = 0.2;
+  aranha.x = 600;
 
-  groundInvisive = createSprite(620, 200, 20, 20);
-  groundInvisive.visible = false;
+  chao = createSprite(250, 600, 200, 200);
+  chao.addImage("chao", chaoImg);
+  chao.scale = 0.8;
+  chao.x = 600;
+
+  chaoInvisivel = createSprite(600, 570, 400, 5);
+  chaoInvisivel.visible = false;
 }
-
 function draw() {
-  background("#341CD0");
+  background("#7EB2DD");
 
-  if (estado === "play") {
-    if (keyDown("space")) {
-      aranha.velocityY = -3;
-    }
+  fill("#931621");
+  text("Aperta espaço para pular", 530, 100);
 
-    aranha.velocityY = aranha.velocityY + 0.8;
-    aranha.collide(groundInvisive);
+  fill("black");
+  text("HOMEM ARANHA", 30, 30);
+
+  if (chao.x < 0) {
+    chao.x = chao.width / 2;
   }
+
+  if (keyDown("space")) {
+    aranha.velocityY = -10;
+  }
+  aranha.velocityY = aranha.velocityY + 0.8;
+  aranha.collide(chaoInvisivel);
 
   drawSprites();
 }
